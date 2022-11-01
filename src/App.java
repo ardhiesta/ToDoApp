cf8dfedcdb56543f7b6603dce1ed97fe9fbf8d31
Stackofsugar <digno.christopher@gmail.com>
2022-11-01 09:57:35 +0700

    Melakukan revisi pada kode utama

--- src/App.java
+++ src/App.java
@@ -2,8 +2,5 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Selamat datang di Program ToDoApp, silakan pilih menu");
         System.out.println("");
-        System.out.println("1. Insert Task");
-        System.out.println("2. Edit Task");
-        System.out.println("3. Search Task");
     }
 }
