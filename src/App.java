79baa75b38567b766441ee3bb330161a8888f35d
Stackofsugar <digno.christopher@gmail.com>
2022-11-01 09:50:35 +0700

    Menyelesaikan issue #1: Menu Utama

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Selamat datang di Program ToDoApp, silakan pilih menu");
+        System.out.println("");
+        System.out.println("1. Insert Task");
+        System.out.println("2. Edit Task");
+        System.out.println("3. Search Task");
     }
 }
