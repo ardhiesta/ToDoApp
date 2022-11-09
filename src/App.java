0c709a5e20b7e117647fad95dd71b5f339c13592
Haizk <haizkb@gmail.com>
2022-11-09 16:23:53 +0700

    Menu Utama

--- src/App.java
+++ src/App.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        /* Menggunakan ANSI
+        System.out.print("\033[1;93m @@@@@@@@@@@@@@@@@@@@@@@@@\n"
+                       + "@@@@@@@@@@@@@@@@@@@@@@@@@@@\n"
+                       + "@@@@@@@@@@@@@@@@@@@@@@@@@@@\033[0m\033[32m ToDoApp\n"
+                       + "\033[0m\033[1;93m@@@@@@@@@@@@@@@@@@\033[0m\033[36m`````\033[0m\033[1;93m@@@@\n"
+                       + "@@@@@@@@@@@@@@@\033[0m\033[36m```````\033[0m\033[1;93m@@@@@\n"
+                       + "@@@@\033[0m\033[36m``````\033[0m\033[1;93m@@@\033[0m\033[36m``````\033[0m\033[1;93m@@@@@@@@\033[0m\033[32m 1. Insert Task\n"
+                       + "\033[0m\033[1;93m@@@@@@\033[0m\033[36m```````````\033[0m\033[1;93m@@@@@@@@@@\033[0m\033[32m 2. Edit Task\n"
+                       + "\033[0m\033[1;93m@@@@@@@@\033[0m\033[36m``````\033[0m\033[1;93m@@@@@@@@@@@@@\033[0m\033[32m 3. Search Task\n"
+                       + "\033[0m\033[1;93m@@@@@@@@@@@@@@@@@@@@@@@@@@@\n"
+                       + "@@@@@@@@@@@@@@@@@@@@@@@@@@@\n"
+                       + " @@@@@@@@@@@@@@@@@@@@@@@@@ \033[0m \033[32mInput: \033[0m");
+        */
+
+        System.out.print(" @@@@@@@@@@@@@@@@@@@@@@@@@\n"
+                       + "@@@@@@@@@@@@@@@@@@@@@@@@@@@\n"
+                       + "@@@@@@@@@@@@@@@@@@@@@@@@@@@ ToDoApp\n"
+                       + "@@@@@@@@@@@@@@@@@@`````@@@@\n"
+                       + "@@@@@@@@@@@@@@@```````@@@@@\n"
+                       + "@@@@``````@@@``````@@@@@@@@ 1. Insert Task\n"
+                       + "@@@@@@```````````@@@@@@@@@@ 2. Edit Task\n"
+                       + "@@@@@@@@``````@@@@@@@@@@@@@ 3. Search Task\n"
+                       + "@@@@@@@@@@@@@@@@@@@@@@@@@@@\n"
+                       + "@@@@@@@@@@@@@@@@@@@@@@@@@@@\n"
+                       + " @@@@@@@@@@@@@@@@@@@@@@@@@  Input: ");
+        
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        if(input.charAt(0) == '1') System.out.println("\nAnda memilih Insert Task");
+        else if(input.charAt(0) == '2') System.out.println("\nAnda memilih Edit Task");
+        else if(input.charAt(0) == '3') System.out.println("\nAnda memilih Search Task");
+        else System.out.println("Input invalid!");
+        
+        scanner.close();
     }
 }
