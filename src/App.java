27b6909b897f3e14b86526f39471ccaec74a8ba7
ardhiesta <ardhi.mail@gmail.com>
2022-11-12 21:57:21 +0700

    refractor code, jadi modular di beberapa method

--- src/App.java
+++ src/App.java
@@ -7,49 +7,47 @@ import java.sql.Statement;
 public class App {
     static Connection cxn = null;
     static Statement stmt = null;
-    public static void main(String[] args) throws Exception {
+
+    // method untuk menampilkan menu
+    public void showMenu() {
+        System.out.println("Selamat datang di Program ToDoApp, silakan pilih menu");
+        System.out.println("1. Insert Task\n"
+                + "2. Edit Task\n"
+                + "3. Search Task\n");
+    }
+
+    // method untuk menghubungkan program ke database sqlite
+    public void connectDb() {
         try {
             cxn = DriverManager.getConnection("jdbc:sqlite:sqlitedb/todo.db");
             System.out.println("Koneksi ke database SQLite: " + (cxn != null) + "\n");
-
-            /* Menggunakan ANSI
-            System.out.print("\033[1;93m @@@@@@@@@@@@@@@@@@@@@@@@@\n"
-                           + "@@@@@@@@@@@@@@@@@@@@@@@@@@@\n"
-                           + "@@@@@@@@@@@@@@@@@@@@@@@@@@@\033[0m\033[32m ToDoApp\n"
-                           + "\033[0m\033[1;93m@@@@@@@@@@@@@@@@@@\033[0m\033[36m`````\033[0m\033[1;93m@@@@\n"
-                           + "@@@@@@@@@@@@@@@\033[0m\033[36m```````\033[0m\033[1;93m@@@@@\n"
-                           + "@@@@\033[0m\033[36m``````\033[0m\033[1;93m@@@\033[0m\033[36m``````\033[0m\033[1;93m@@@@@@@@\033[0m\033[32m 1. Insert Task\n"
-                           + "\033[0m\033[1;93m@@@@@@\033[0m\033[36m```````````\033[0m\033[1;93m@@@@@@@@@@\033[0m\033[32m 2. Edit Task\n"
-                           + "\033[0m\033[1;93m@@@@@@@@\033[0m\033[36m``````\033[0m\033[1;93m@@@@@@@@@@@@@\033[0m\033[32m 3. Search Task\n"
-                           + "\033[0m\033[1;93m@@@@@@@@@@@@@@@@@@@@@@@@@@@\n"
-                           + "@@@@@@@@@@@@@@@@@@@@@@@@@@@\n"
-                           + " @@@@@@@@@@@@@@@@@@@@@@@@@ \033[0m \033[32mInput: \033[0m");
-            */
-
-            System.out.print(" @@@@@@@@@@@@@@@@@@@@@@@@@\n"
-                           + "@@@@@@@@@@@@@@@@@@@@@@@@@@@\n"
-                           + "@@@@@@@@@@@@@@@@@@@@@@@@@@@ ToDoApp\n"
-                           + "@@@@@@@@@@@@@@@@@@`````@@@@\n"
-                           + "@@@@@@@@@@@@@@@```````@@@@@\n"
-                           + "@@@@``````@@@``````@@@@@@@@ 1. Insert Task\n"
-                           + "@@@@@@```````````@@@@@@@@@@ 2. Edit Task\n"
-                           + "@@@@@@@@``````@@@@@@@@@@@@@ 3. Search Task\n"
-                           + "@@@@@@@@@@@@@@@@@@@@@@@@@@@\n"
-                           + "@@@@@@@@@@@@@@@@@@@@@@@@@@@\n"
-                           + " @@@@@@@@@@@@@@@@@@@@@@@@@  Input: ");
-            
-            Scanner scanner = new Scanner(System.in);
-            String input = scanner.nextLine();
-
-            if(input.charAt(0) == '1') System.out.println("\nAnda memilih Insert Task");
-            else if(input.charAt(0) == '2') System.out.println("\nAnda memilih Edit Task");
-            else if(input.charAt(0) == '3') System.out.println("\nAnda memilih Search Task");
-            else System.out.println("\nInput invalid!");
-
-            scanner.close();
-            cxn.close();
-        } catch(SQLException e) {
-            System.out.println(e.getClass().getName() + ": " + e.getMessage());
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        App app = new App();
+        app.connectDb();
+        app.showMenu();
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        if (input.charAt(0) == '1')
+            System.out.println("\nAnda memilih Insert Task");
+        else if (input.charAt(0) == '2')
+            System.out.println("\nAnda memilih Edit Task");
+        else if (input.charAt(0) == '3')
+            System.out.println("\nAnda memilih Search Task");
+        else
+            System.out.println("\nInput invalid!");
+
+        scanner.close();
+        cxn.close();
+
+        // TODO:
+        // menambahkan input, edit, dan search tasks
     }
 }
