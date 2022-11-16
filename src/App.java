7977a4908fd12968b56f64f024a18b17efc51d2f
Alfiki Diastama Afan Firdaus <89908618+alfikiafan@users.noreply.github.com>
2022-11-16 09:59:59 +0700

    Memperbarui regex
    Menyesuaikan aturan jumlah tanggal di bulan tertentu, dan tahun kabisat

--- src/App.java
+++ src/App.java
@@ -44,7 +44,10 @@ public class App {
         while (true){
             System.out.print("Tuliskan deadline tugas (YYYY-MM-DD): ");
             String deadline = scanner.nextLine();
-            String regex = "^((19|2[0-9])[0-9]{2})-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$";
+            String regex = "^((2000|2400|2800|(19|2[0-9])(0[48]|[2468][048]|[13579][26]))-02-29)$" 
+      + "|^(((19|2[0-9])[0-9]{2})-02-(0[1-9]|1[0-9]|2[0-8]))$"
+      + "|^(((19|2[0-9])[0-9]{2})-(0[13578]|10|12)-(0[1-9]|[12][0-9]|3[01]))$" 
+      + "|^(((19|2[0-9])[0-9]{2})-(0[469]|11)-(0[1-9]|[12][0-9]|30))$";
             Pattern pattern = Pattern.compile(regex);
             Matcher matcher = pattern.matcher(deadline);
             if (matcher.matches()) {
